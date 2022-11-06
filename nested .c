c77742ec237c12aff39522239672a2186194f295
Harshh123 <116649572+Harshh123@users.noreply.github.com>
2022-11-06 13:10:26 +0530

    Add files via upload

---
+++ nested .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three number");
+scanf("%d%d%d",&a,&b,&c);
+if (a>b)
+{
+if (a>c)
+{
+printf("greater number=%d",a);
+if(b=c)
+printf("lowest number=%d",b);
+}
+else
+{
+printf("lowest number=%d",c);
+}
+}
+else
+{
+printf
